7c310383415251b2a943206138c1750bb00fec3b
TechInceptions <TechInceptions@users.noreply.github.com>
2021-10-28 08:56:38 -0400

    removed old test project updated package dependency updated versions

--- TECHIS.CloudFile.AzureStorage.Test/TestBlobReaderWriter.cs
+++ TECHIS.CloudFile.AzureStorage.Test/TestBlobReaderWriter.cs
@@ -2,23 +2,24 @@
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using TECHIS.CloudFile.AzureStorage;
+using Xunit;
 
 namespace Test.Cloud.AzureStorage
 {
-    [TestClass]
+
     public class TestBlobReaderWriter
     {
-        [TestMethod]
+        [Fact]
         public void ReadText()
         {
             string data = (new ReaderFactory()).Connect(GetContainerUri()).ReadText(FixedFileName);
 
-            Assert.IsFalse(string.IsNullOrEmpty(data), "Failed to read blob file");
+            Assert.False(string.IsNullOrEmpty(data), "Failed to read blob file");
         }
 
-        [TestMethod]
+        [Fact]
         public void WriteText()
         {
             
@@ -27,9 +28,9 @@ namespace Test.Cloud.AzureStorage
             byte[] data = System.Text.Encoding.UTF8.GetBytes("Test data");
             (new WriterFactory()).Connect(containerUri).WriteToBlob(data, fileName);
 
-            //Assert.IsFalse(string.IsNullOrEmpty(data), "Failed to read blob file");
+            //Assert.False(string.IsNullOrEmpty(data), "Failed to read blob file");
         }
-        [TestMethod]
+        [Fact]
         public void ReadTextAsync()
         {
             
@@ -37,10 +38,10 @@ namespace Test.Cloud.AzureStorage
             var fileName = FixedFileName;
             string data = (new ReaderFactory()).Connect(containerUri).ReadTextAsync(fileName).Result;
 
-            Assert.IsFalse(string.IsNullOrEmpty(data), "Failed to read blob file");
+            Assert.False(string.IsNullOrEmpty(data), "Failed to read blob file");
         }
-        [TestMethod]
-        public void ReadDataAsync()
+        [Fact]
+        public async Task ReadDataAsync()
         {
             var containerUri = GetContainerUri();
             Encoding e = Encoding.UTF8;
@@ -48,16 +49,15 @@ namespace Test.Cloud.AzureStorage
             var fileName = FixedFileName;
             string data = null;
 
-            Task task = null;
             using (MemoryStream ms = new MemoryStream())
             {
-                task = br.ReadDataAsync(fileName, ms);
-                task.Wait();
+                await br.ReadDataAsync(fileName, ms);
+                
                 data = new string(e.GetChars(ms.ToArray()));
             }
-            Assert.IsFalse(string.IsNullOrEmpty(data), $"Failed to read blob file via {nameof(ReadDataAsync)}");
+            Assert.False(string.IsNullOrEmpty(data), $"Failed to read blob file via {nameof(ReadDataAsync)}");
         }
-        [TestMethod]
+        [Fact]
         public void ReadData()
         {
             var containerUri = GetContainerUri();
@@ -71,29 +71,29 @@ namespace Test.Cloud.AzureStorage
                 br.ReadData(fileName, ms);
                 data = new string(e.GetChars(ms.ToArray()));
             }
-            Assert.IsFalse(string.IsNullOrEmpty(data), $"Failed to read blob file via {nameof(ReadData)}");
+            Assert.False(string.IsNullOrEmpty(data), $"Failed to read blob file via {nameof(ReadData)}");
         }
 
-        [TestMethod]
-        public void WriteTextAsync()
+        [Fact]
+        public async Task WriteTextAsync()
         {
             string containerUri = GetContainerUri();
             var br = new WriterFactory().Connect(containerUri);
             var fileName = "l1/l3";
             byte[] data = System.Text.Encoding.UTF8.GetBytes($"Test data: {DateTime.UtcNow.ToLongTimeString()}");
             var task = br.WriteToBlobAsync(data, fileName);
-            task.Wait();
-            Assert.IsTrue(task.IsCompleted, "Failed to Write Async blob file");
+            await task;
+            Assert.True(task.IsCompleted, "Failed to Write Async blob file");
         }
-        [TestMethod]
-        public void WriteTextAsync2()
+        [Fact]
+        public async Task WriteTextAsync2()
         {
             var br = new WriterFactory().Connect(StorageConnectionString, "test");
             var fileName = "l1/l3";
             byte[] data = System.Text.Encoding.UTF8.GetBytes($"Test data: {DateTime.UtcNow.ToLongTimeString()}");
             var task = br.WriteToBlobAsync(data, fileName);
-            task.Wait();
-            Assert.IsTrue(task.IsCompleted, "Failed to Write Async blob file");
+            await task;
+            Assert.True(task.IsCompleted, "Failed to Write Async blob file");
         }
 
         private string FixedFileName => "FixedDirectory/S2715H.inf";
@@ -103,7 +103,8 @@ namespace Test.Cloud.AzureStorage
             return "https://tests4dev.blob.core.windows.net/cloudfile?sv=2015-12-11&si=cloudfile-RWLD&sr=c&sig=qXnlg3DGNBrT8wWVAeeeqn8asP%2BJXjYdXH1os6mdaCU%3D";
         }
 
-        private string StorageConnectionString => "SharedAccessSignature=sv=2016-05-31&ss=b&srt=co&sp=rwdl&st=2017-05-05T17%3A36%3A00Z&se=2018-05-06T17%3A36%3A00Z&sig=4qZAoOGRiuwK9THxy%2BTH4tLznQt%2FZIRESPvg%2Bc8qsTA%3D;BlobEndpoint=https://tests4dev.blob.core.windows.net/";
+        //private string StorageConnectionString => "SharedAccessSignature=sv=2016-05-31&ss=b&srt=co&sp=rwdl&st=2017-05-05T17%3A36%3A00Z&se=2018-05-06T17%3A36%3A00Z&sig=4qZAoOGRiuwK9THxy%2BTH4tLznQt%2FZIRESPvg%2Bc8qsTA%3D;BlobEndpoint=https://tests4dev.blob.core.windows.net/";
+          private string StorageConnectionString => "SharedAccessSignature=sv=2020-04-08&ss=btqf&srt=sco&st=2021-10-26T11%3A48%3A00Z&se=2040-10-29T11%3A48%3A00Z&sp=rwdxftlacup&sig=d02Dv2Xg03j8dZT1oX0gt6FmmF5jk88TXjOIbqNLgzA%3D;BlobEndpoint=https://tests4dev.blob.core.windows.net/;FileEndpoint=https://tests4dev.file.core.windows.net/;QueueEndpoint=https://tests4dev.queue.core.windows.net/;TableEndpoint=https://tests4dev.table.core.windows.net/;";
 
     }
 }
